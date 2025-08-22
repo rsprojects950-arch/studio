5d682a40490e1718f1b6a375d22461d84b0a7912
Sriram Neppalli <sriramps31@gmail.com>
2025-08-22 14:07:07 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,13 +5,13 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  "projectId": "beyond-theory-nnj8t",
-  "appId": "1:802409678105:web:398aeeac8f9df7873b3188",
-  "storageBucket": "beyond-theory-nnj8t.appspot.com",
-  "apiKey": "AIzaSyB0arME69fy4Aa0HQf8xSvdmr84VSiFvp4",
-  "authDomain": "beyond-theory-nnj8t.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "802409678105"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
